3da4b2481401d6e71db1772e935d69fdad338b1d
George <g.andronchik@gmail.com>
2022-05-24 02:18:49 +0800

    chore: minor

--- rust/cubesql/cubesql/src/sql/statement.rs
+++ rust/cubesql/cubesql/src/sql/statement.rs
@@ -679,6 +679,9 @@ impl<'ast> Visitor<'ast> for SensitiveDataSanitizer {
             ast::Value::SingleQuotedString(str)
             | ast::Value::DoubleQuotedString(str)
             | ast::Value::NationalStringLiteral(str) => {
+                if vec!["f", "false", "t", "true"].contains(&str.as_str()) {
+                    return;
+                }
                 *str = "[REPLACED]".to_string();
             }
             _ => (),
@@ -872,7 +875,7 @@ mod tests {
     #[test]
     fn test_sensitive_data_sanitizer() -> Result<(), CubeError> {
         assert_sensitive_data_sanitizer(
-            "SELECT * FROM testdata WHERE email = 'george@gmail.com'",
+            "SELECT * FROM testdata WHERE email = 'to@replace.com'",
             "SELECT * FROM testdata WHERE email = '[REPLACED]'",
         )?;
 
